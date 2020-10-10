b27b26f16d0ca00e1747c467c65de461a0107411
Vishal Ramane <vramane007@gmail.com>
2020-10-10 17:55:12 +0530

    pose estimation basic code

---
+++ model.js
@@ -0,0 +1,58 @@
+let video;
+let poseNet;
+let pose;
+let skeleton;
+
+function setup(){
+    createCanvas(640,480);
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video,onModelLoad);
+    poseNet.on('pose',gotPose);
+}
+
+function gotPose(poses){
+    console.log(poses);
+    if(poses.length >0){
+        pose = poses[0].pose;
+        skeleton = poses[0].skeleton;
+    }
+}
+
+function onModelLoad(){
+    console.log('posenet model ready');
+}
+
+function draw(){
+    image(video,0,0);
+    if(pose){
+        
+        // // mark nose
+        // fill(255,0,0);
+        // ellipse(pose.nose.x,pose.nose.y,25);
+
+        // // mark wrists
+        // fill(0,255,0);
+        // ellipse(pose.leftWrist.x,pose.leftWrist.y,25);
+        // ellipse(pose.rightWrist.x,pose.rightWrist.y,25);
+        
+        // mark all keypoints
+        for(let i = 0; i <pose.keypoints.length;i++){
+            let x = pose.keypoints[i].position.x;
+            let y = pose.keypoints[i].position.y;
+
+            fill(255,0,0);
+            ellipse(x,y,16,16);
+        }
+        
+        // Draw Skeleton
+        for(let i = 0; i <skeleton.length;i++){
+            let a = skeleton[i][0];
+            let b = skeleton[i][1];
+            strokeWeight(2);
+            stroke(255);
+            line(a.position.x,a.position.y,b.position.x,b.position.y);
+        }
+    }
+   
+}
